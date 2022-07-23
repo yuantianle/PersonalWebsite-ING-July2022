ed67ed92b1a6c3f761a1e702d6140a6b6ab2aa67
Tianle Yuan <yuant@oregonstate.edu>
2022-07-23 03:06:50 -0700

    Finished shader model

--- assets/js/Three_js/my_script.js
+++ assets/js/Three_js/my_script.js
@@ -1,12 +1,36 @@
-/*import * as THREE from 'three';
-import {AnaglyphEffect} from 'assets/js/Three_js/effects/AnaglyphEffect.js';
-AnaglyphEffect = require('assets/js/Three_js/effects/AnaglyphEffect.js');
-import {OrbitControls} from 'assets/js/Three_js/controls/OrbitControls.js';
-OrbitControls = require('assets/js/Three_js/controls/OrbitControls.js');
-import Stats from 'assets/jsm/libs/stats.module.js';*/
+var scene, camera, renderer;
+var axes, plane, meshes, material
+var normalMapTexture, textureLoader;
+var controls, stats, gui;
+var nowtime;
 
-var scene,camera,renderer,textureLoader;
-var controls,stats, axes, plane, cube;
+const options = {
+     showAxis: false,
+     enableSwoopingCamera: false,
+     enableRotation: true,
+     transmission: 1,
+     thickness: 1.2,
+     roughness: 0.35,
+     envMapIntensity: 2.5,
+     clearcoat: 1,
+     clearcoatRoughness: 0.1,
+     normalScale: 8,
+     clearcoatNormalScale: 3.14,
+     normalRepeat: 1,
+     bloomThreshold: 0.85,
+     bloomStrength: 0.5,
+     bloomRadius: 0.33,
+};
+
+const positions = [
+     [-0.85, 0.85, 0],
+     [0.85, 0.85, 0],
+];
+ 
+const geometries = [
+     new THREE.IcosahedronGeometry(0.75, 0), // Faceted
+     new THREE.IcosahedronGeometry(0.67, 24), // Sphere
+];
 
 /*-------------------------------
      Initiation
@@ -25,6 +49,7 @@ function init()
      initOthers();
 
      setControl();
+     setGUI();
      setEventsMouse();
      setKeyEvents(); 
      refreshWindown();    
@@ -48,9 +73,9 @@ function initScene()
 function  initCamera()
 {
      camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-     camera.position.x = -5;
-     camera.position.y = 5;
-     camera.position.z = 5
+     camera.position.x = -2;
+     camera.position.y = 3;
+     camera.position.z = 3;
      camera.lookAt(scene.position);
 }
 
@@ -74,7 +99,7 @@ function refreshWindown(){
      //add event monitor, adapt the window
      window.addEventListener('resize', function(){
          var width = window.innerWidth*0.82;
-         var height = window.innerHeight*0.7;
+         var height = window.innerHeight*0.8;
          renderer.setSize(width,height);
          camera.aspect = width/height;
          camera.updateProjectionMatrix();
@@ -84,7 +109,7 @@ function refreshWindown(){
 
 function setWindown(){
      var width = window.innerWidth*0.82;
-     var height = window.innerHeight*0.7;
+     var height = window.innerHeight*0.77;
      renderer.setSize(width,height);
      camera.aspect = width/height;
      camera.updateProjectionMatrix();
@@ -97,10 +122,26 @@ function setWindown(){
 function initLight()
 {
      var spotLight = new THREE.SpotLight( 0xffffff );
-     spotLight.position.set( 0, 60, 0 );
+     spotLight.position.set( 0, 60, 10 );
+     //spotlight resolution
+     spotLight.angle = Math.PI/20;
+     spotLight.shadow.penumbra = 0.05;
+     spotLight.shadow.mapSize.width = 2048;
+     spotLight.shadow.mapSize.height = 2048;
      scene.add( spotLight );
      // Set projection
      spotLight.castShadow = true;
+
+     var spotLight2 = new THREE.SpotLight( 0xffffff );
+     spotLight2.position.set( -10, 30, 0 );
+     //spotlight resolution
+     spotLight2.angle = Math.PI/20;
+     spotLight2.shadow.penumbra = 0.05;
+     spotLight2.shadow.mapSize.width = 2048;
+     spotLight2.shadow.mapSize.height = 2048;
+     scene.add( spotLight2 );
+     // Set projection
+     spotLight2.castShadow = true;
 }
 
 function initTextureLoader()
@@ -108,57 +149,37 @@ function initTextureLoader()
      textureLoader = new THREE.TextureLoader();
 }
 
+
 /*-------------------------------
      Geometry
 --------------------------------*/
 function setGeometrys()
 {
      //---1.Axis-----------------------
-     axes = new THREE.AxesHelper(20);
+     axes = new THREE.AxesHelper(5);
      scene.add(axes);
 
      //---2.Relative ground-------------
      var planeGeometry = new THREE.PlaneGeometry(10, 10, 1, 1);
-     const planeTexture = textureLoader.load("assets/js/Three_js/mark.jpg");
-     const planeMaterial = new THREE.MeshPhongMaterial({ map: planeTexture, color: 0xffffff, specular:0x4488ee, shininess:12 });
+     const planeTexture = textureLoader.load("assets/js/Three_js/texture/mark.jpg");
+     const planeMaterial = new THREE.MeshPhongMaterial({ map: planeTexture, color: 0xffffff, shininess:40});
      plane = new THREE.Mesh(planeGeometry, planeMaterial);
      
      plane.rotation.x = -0.5 * Math.PI;
-     plane.position.x = 0
-     plane.position.y = 0
-     plane.position.z = 0
+     plane.rotation.z = 0.25 * Math.PI;
+     plane.position.set(0, -2, -2);
      scene.add(plane);
      // Set projection
      plane.receiveShadow = true;
 
      //---3.Object-----------------------
-     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-     
-     const options = {
-          enableSwoopingCamera: false,
-          enableRotation: true,
-          transmission: 1,
-          thickness: 1.2,
-          roughness: 0.6,
-          envMapIntensity: 1.5,
-          clearcoat: 1,
-          clearcoatRoughness: 0.1,
-          normalScale: 1,
-          clearcoatNormalScale: 0.3,
-          normalRepeat: 1,
-          bloomThreshold: 0.85,
-          bloomStrength: 0.5,
-          bloomRadius: 0.33,
-        };
-
-     //var material = new THREE.MeshPhongMaterial( { color: 0x00ffff, specular:0x4488ee, shininess:12} );
-     const normalMapTexture = textureLoader.load("assets/js/Three_js/noise_normal.jpg");
+     normalMapTexture = textureLoader.load("assets/js/Three_js/texture/noise_normal.jpg");
      normalMapTexture.wrapS = THREE.RepeatWrapping;
      normalMapTexture.wrapT = THREE.RepeatWrapping;
      normalMapTexture.repeat.set(options.normalRepeat, options.normalRepeat);
      
      const hdrEquirect = new THREE.RGBELoader().load(
-          "assets/js/Three_js/royal_esplanade_4k.hdr",
+          "assets/js/Three_js/texture/royal_esplanade_4k.hdr",
           () => {
             hdrEquirect.mapping = THREE.EquirectangularReflectionMapping;
             scene.background = hdrEquirect;
@@ -166,7 +187,7 @@ function setGeometrys()
           }
         );
      
-     var material = new THREE.MeshPhysicalMaterial({
+     material = new THREE.MeshPhysicalMaterial({
           transmission: options.transmission,
           thickness: options.thickness,
           roughness: options.roughness,
@@ -179,13 +200,16 @@ function setGeometrys()
           clearcoatNormalMap: normalMapTexture,
           clearcoatNormalScale: new THREE.Vector2(options.clearcoatNormalScale),
      });
-     cube = new THREE.Mesh( geometry, material );
-     cube.position.x = 0;
-     cube.position.y = 2;
-     cube.position.z = 0;
-     scene.add( cube );
-     // Set projection
-     cube.castShadow  = true;
+
+     meshes = geometries.map(
+          (geometry) => new THREE.Mesh(geometry, material)
+     );
+     meshes.forEach((mesh, i) => {
+          scene.add(mesh);
+          mesh.position.set(...positions[i]);
+          // Set projection
+          mesh.castShadow = true;
+     });
 }
 
 function initOthers()
@@ -201,6 +225,7 @@ function setControl()
 {
      //orbit for camera movement
      controls = new THREE.OrbitControls(camera,renderer.domElement);
+     controls.enabled = !options.enableSwoopingCamera;
      controls.addEventListener('change', () => {
           renderer.render(scene, camera)
      })
@@ -211,15 +236,63 @@ function setControl()
 //add GUI
 function setGUI()
 {
-     const gui = new GUI()
-     const cubeFolder = gui.addFolder('cube')
-     cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-     cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-     cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-     cubeFolder.open()
-     const cameraFolder = gui.addFolder('camera')
-     cameraFolder.add(camera.position, 'z', 0, 10)
-     cameraFolder.open()
+     gui = new dat.GUI({ autoPlace: false });
+
+     //erase function:
+     //cube.rotation.set(0, 0, 0);
+
+     gui.add(options, "showAxis").onChange((val) => {
+          options.axis = val;
+     });
+
+     gui.add(options, "enableSwoopingCamera").onChange((val) => {
+          controls.enabled = !val;
+          controls.reset();
+     });
+      
+     gui.add(options, "enableRotation").onChange(() => {
+     });
+      
+     gui.add(options, "transmission", 0, 1, 0.01).onChange((val) => {
+          material.transmission = val;
+     });
+      
+     gui.add(options, "thickness", 0, 5, 0.1).onChange((val) => {
+          material.thickness = val;
+     });
+      
+     gui.add(options, "roughness", 0, 1, 0.01).onChange((val) => {
+          material.roughness = val;
+     });
+      
+     gui.add(options, "envMapIntensity", 0, 3, 0.1).onChange((val) => {
+          material.envMapIntensity = val;
+     });
+      
+     gui.add(options, "clearcoat", 0, 1, 0.01).onChange((val) => {
+          material.clearcoat = val;
+     });
+      
+     gui.add(options, "clearcoatRoughness", 0, 1, 0.01).onChange((val) => {
+          material.clearcoatRoughness = val;
+     });
+      
+     gui.add(options, "normalScale", 0, 10, 0.01).onChange((val) => {
+          material.normalScale.set(val, val);
+     });
+      
+     gui.add(options, "clearcoatNormalScale", 0, 5, 0.01).onChange((val) => {
+          material.clearcoatNormalScale.set(val, val);
+     });
+      
+     gui.add(options, "normalRepeat", 1, 4, 1).onChange((val) => {
+          normalMapTexture.repeat.set(val, val);
+     });
+      
+     const postprocessing = gui.addFolder("Post Processing");
+     postprocessing.open();
+     document.getElementById('webgl-output').appendChild(gui.domElement);
+     gui.domElement.id = 'guiContainer';
 }
 //Define mouse event
 function setEventsMouse(){
@@ -254,18 +327,43 @@ function setKeyEvents(){
 function setStatusMontor()
 {
      stats = new Stats();
-     stats.showPanel( 0 );
-     document.body.appendChild( stats.domElement);
+     stats.setMode( 0 );
+     stats.domElement.style.position = 'absolute';
+     stats.domElement.style.left = '0px';
+     stats.domElement.style.top = '0px';
+     document.getElementById('statsContainer').appendChild(stats.domElement);
 }
 
 /*-------------------------------
      Logic & Animate
 --------------------------------*/
 //Logic
-var update=function()
+var update=function(deltaTime)
 {
-    	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+     const ROTATE_TIME = 5000; // Time in seconds for a full rotation
+     const xAxis = new THREE.Vector3(1, 0, 0);
+     const yAxis = new THREE.Vector3(0, 1, 0);
+     const rotateX = (deltaTime / ROTATE_TIME) * Math.PI * 2;
+     const rotateY = (deltaTime / ROTATE_TIME) * Math.PI * 2;
+
+     if(options.enableSwoopingCamera)
+     {
+          camera.position.x = Math.sin((nowtime / ROTATE_TIME) * Math.PI * 2) * 2;
+          camera.position.y = Math.cos((nowtime / ROTATE_TIME) * Math.PI * 2) * 2;
+          camera.position.z = 5;
+          camera.lookAt(scene.position);
+     }
+     if(options.enableRotation)
+    	{
+          meshes.forEach((mesh) => {
+               mesh.rotateOnWorldAxis(xAxis, rotateX);
+               mesh.rotateOnWorldAxis(yAxis, rotateY);
+          });
+     }
+     if (options.axis == true)
+          scene.add(axes);
+     else
+          scene.remove(axes);
      //controls.update();
 }
 
@@ -273,20 +371,20 @@ var update=function()
 var animate=function() {
       //render 3D scene
      renderer.render( scene, camera );     
-
      //render 3D effect
      //effect.render(scene,camera); 
 }
 
 //Loop update and render
-let T0 = new Date(); // the last time T0
+nowtime = new Date(); // the last time T0
 var loop=function() {     
      //animated object
      let T1 = new Date(); // the time at this moment
-     let t = T1 - T0;
-     T0 = T1;
+     let t = T1 - nowtime;
+     nowtime = T1;
      requestAnimationFrame(loop);
-     update();
+     controls.update();
+     update(t);
      animate();
      stats.update();
 }
